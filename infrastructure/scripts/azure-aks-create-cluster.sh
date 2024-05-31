305f02f897c6c3c01903f39eadf4759b41866a28
Liam Vaessen <liamvaessen@gmail.com>
2024-05-31 11:31:10 +0200

    Release v1.2
    - Changed configuration for cluster creation from single-node cluster to 2 nodes (maximum of subscription)

--- infrastructure/scripts/azure-aks-create-cluster.sh
+++ infrastructure/scripts/azure-aks-create-cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-az aks create --resource-group enableResourceGroupEastUS --name enable --node-count 1 --generate-ssh-keys
+az aks create --resource-group enableResourceGroupEastUS --name enable --node-count 2 --generate-ssh-keys
 az aks get-credentials --resource-group enableResourceGroupEastUS --name enable
